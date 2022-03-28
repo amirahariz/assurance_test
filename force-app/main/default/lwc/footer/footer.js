f6e208b0f814aa746445991ea0f4bd46013cdac8
betbout <betboutaziz1@gmail.com>
2022-03-28 10:12:29 +0100

    test

---
+++ force-app/main/default/lwc/footer/footer.js
@@ -0,0 +1,14 @@
+import { LightningElement } from 'lwc';
+import CAROUSEL_IMAGES from '@salesforce/resourceUrl/iconfooter';
+export default class Footer extends LightningElement {
+
+
+    imagefacebook=`${CAROUSEL_IMAGES}/facebook.png`;
+    imagetwitter=`${CAROUSEL_IMAGES}/twitter.png`;
+    imageinstagram=`${CAROUSEL_IMAGES}/instagram.png`;
+    imagegoogleplus=`${CAROUSEL_IMAGES}/google-plus.png`;
+    imagehouse=`${CAROUSEL_IMAGES}/house.png`;
+    imagesmartphone=`${CAROUSEL_IMAGES}/smartphone.png`;
+    imagecontact=`${CAROUSEL_IMAGES}/contact.png`;
+
+}
